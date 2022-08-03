12a7a4a228dcc7b2431f2475525324d5270413b1
Gdawson771 <gdawson771@gmail.com>
2022-08-03 14:51:04 +0100

    fixed fetch link

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -51,7 +51,7 @@ export default function Form() {
             }
           })*/
         
-                fetch("https://w5jn79ev5c.sexecute-api.eu-west-2.amazonaws.com/sendEmail",
+                fetch("https://w5jn79ev5c.execute-api.eu-west-2.amazonaws.com/sendEmail",
                     {
                         method: "POST",
                         mode: "no-cors",
