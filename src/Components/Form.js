a5d4972b76decb0b72c65a9fc9ea772738c8a58a
Gdawson771 <gdawson771@gmail.com>
2022-08-03 14:49:46 +0100

    implemented useNavigate

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,9 @@
 import React from "react"
+import { useNavigate } from "react-router-dom";
 
 export default function Form() {
+    
+ let navigate = useNavigate();
     const [formData, setFormData] = React.useState(
         { email: "", text: "" }
     )
@@ -12,6 +15,11 @@ export default function Form() {
             ...prevData,
             [event.target.name]: event.target.value
         }))
+    }
+    function submitEmail(){
+     //   const navigate = useNavigate();
+     navigate('/')
+                  
     }
     function submitData(event) {
         
@@ -43,7 +51,7 @@ export default function Form() {
             }
           })*/
         
-                fetch("https://w5jn79ev5c.execute-api.eu-west-2.amazonaws.com/sendEmail",
+                fetch("https://w5jn79ev5c.sexecute-api.eu-west-2.amazonaws.com/sendEmail",
                     {
                         method: "POST",
                         mode: "no-cors",
@@ -104,15 +112,15 @@ export default function Form() {
 +                <section >
 +                        <p>Message Sent! Thanks!</p>
 +
-+                    <h5>Email: {formData.email}</h5>
++                    <h5>Email: {formData.email}</h5>   
 +                    <h5>Message: {formData.text}</h5>
 +
 +
 +                </section>
 +
-+                    <button onCLick={()=>setEmailSent(false)} >Confirm</button>
++                   <button className="Form--btn" onClick={submitEmail} >Confirm</button>  
 +
-+                </div>}
++                </div>} 
         </form>
     )
 }
